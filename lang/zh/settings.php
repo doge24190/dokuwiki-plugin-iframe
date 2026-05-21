d0377c97f543fa97ce4988cf41484b63f3c06809
doge24190 <69849296+doge24190@users.noreply.github.com>
2026-05-22 03:22:40 +0800

    add Chinese language support

---
+++ lang/zh/settings.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Chinese language file
+ *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     doge24190 <me@doge24190.top>
+ */
+ 
+// for the configuration manager
+$lang['js_ok']  = '允许含有javascript的url链接';
